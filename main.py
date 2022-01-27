2528580ddc70ef39b33668fc932737426702e103
abatyuk <andrii-batiuk@gmail.com>
2022-01-26 23:09:34 -0800

    commit

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-apt - cache madison docker - ce
 
-sudo
-apt - get
-install
-docker - ce = < VERSION_STRING > docker - ce - cli = < VERSION_STRING > containerd.io
+
 
