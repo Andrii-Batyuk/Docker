b8d0cba4f6cc1ce64bc0239a26b4d1b881003fb6
Andrii-Batyuk <67364790+Andrii-Batyuk@users.noreply.github.com>
2022-02-03 15:17:07 +0200

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,6 +1,9 @@
 # https://docs.docker.com/engine/install/ubuntu/
 
+# Updating system
 sudo apt-get update
+sudo apt-get upgrade -y
+
 sudo apt-get install \
     ca-certificates \
     curl \
@@ -11,6 +14,8 @@ echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+
+#Installing Docker
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
